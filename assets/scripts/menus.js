4c007d25fd8583c83376115a2c0aac6c40b6839c
Felix Lindh <felix.lindh@student.kyh.se>
2022-12-28 16:27:37 +0100

    started implementing see-todos

--- assets/scripts/menus.js
+++ assets/scripts/menus.js
@@ -6,7 +6,7 @@ function setMainMenu() {
           <h2 class="menu-title">Todo List</h2>
           <div class="menu-buttons">
               <button onclick="setCreateTodoMenu()">Skapa Todo</button>
-              <button>Se Todos</button>
+              <button onclick="seeTodos()">Se Todos</button>
               <button>Leta Todos</button>
           </div>
       </div>
@@ -20,16 +20,37 @@ function setCreateTodoMenu() {
     <input class="title-input"/>
     <input class="content-input"/>
     <input class="end-date-input"/>
-    <button>Skapa todo</button>
+    <button onClick="onCreateTodoClick(event)">Skapa todo</button>
     <button onclick="setMainMenu()">HuvudMeny</button>
     `;
-  pageMenu.querySelectorAll("button")[0].addEventListener("click", (event) => {
-    const inputs = event.currentTarget.parentElement.querySelectorAll("input");
+    console.log("setting click event");
+}
+
+function onCreateTodoClick(event) {
+  const inputs = event.currentTarget.parentElement.querySelectorAll("input");
     const todoItem = new TodoItem(
       inputs[0].value,
       inputs[1].value,
       inputs[2].value
     );
     todoList.addTodoItem(todoItem);
-  });
+    console.table(todoList)
+}
+
+
+function seeTodos() {
+  const pageMenu = container.querySelector(".page-menu");
+  const listContainer = document.createElement("div");
+  console.table(todoList)
+  for (let i = 0; i < todoList.todos.length; i++) {
+    const listItemContainer = document.createElement("div");
+    listItemContainer.innerHTML = 
+    `<h1>${todoList.todos[i].title}<h1>
+     <p>${todoList.todos[i].content}</p>
+     <p>${todoList.todos[i].startDate}</p>
+     <p>${todoList.todos[i].endDate}</p>`;
+
+     listContainer.append(listItemContainer);
+  }
+  pageMenu.innerHTML = listContainer.innerHTML ;
 }
